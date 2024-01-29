ebf5c28e0b57e6e064c685fd63960907284a7e04
TohidEq <TohidEq@gmail.com>
2024-01-29 21:10:07 +0330

    Create Inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
 "use client";
 
 import generateRandomSha256 from "@/lib/generateRandomSha256";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Home() {
   const randomSha256 = generateRandomSha256();
-  const [guess, setGuess] = useState<string>();
+  const [guess, setGuess] = useState<any>({ "1": 123 });
+  const myRef = useRef<any>();
   console.log(randomSha256);
+
+  const changeControll = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const inputId = Number(e.currentTarget.id);
+    console.log(inputId);
+    if (e.currentTarget.value) {
+      guess[`${inputId}`] = e.currentTarget.value;
+      document.getElementById((inputId + 1).toString())?.focus();
+      document.getElementById((inputId + 1).toString())?.select();
+
+      console.log(guess);
+    }
+  };
+
+  const allInputs = [];
+
+  for (let i = 0; i < 64; i++) {
+    const input = (
+      <input
+        key={i}
+        type="text"
+        maxLength={1}
+        onKeyUp={changeControll}
+        id={`${i}`}
+        placeholder={`${i % 2 ? "O" : "X"}`}
+        required
+      />
+    );
+    allInputs[i] = input;
+  }
 
   return (
     <main className="Home">
-      <p>hello, this is Home</p>
+      <form action="" ref={myRef}>
+        {allInputs.map((input) => input)}
+        <button type="submit">check</button>
+      </form>
     </main>
   );
 }
